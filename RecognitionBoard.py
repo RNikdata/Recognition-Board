079374ea356455a71799d1b13b86e5c59c0d32b5
RNikdata <162301716+RNikdata@users.noreply.github.com>
2025-12-15 13:52:55 +0530

    Update RecognitionBoard.py

--- RecognitionBoard.py
+++ RecognitionBoard.py
@@ -764,7 +764,7 @@ elif st.session_state.get("active_page") == "Final Display Board":
                     
                     winners_html += "<div style='width:110px; display:flex; flex-direction:column; align-items:center; text-align:center;'>"
                     if w.get('photo', "") != "":
-                        winners_html += f"<img src='{w['photo']}' style='width:80px; height:80px; border-radius:50%; object-fit:cover; border:2px solid #fff; margin-bottom:5px;'>"
+                        winners_html += f""" <img src='{w['photo']}' style='width:80px; height:80px; border-radius:50%;object-fit:cover; border:2px solid #fff; margin-bottom:5px;'> """
                     winners_html += f"<div style='font-size:12px;color:#888888;font-weight:bold; text-align:center;'>{w['name']}</div>"
                     winners_html += f"<div style='font-size:11px;  color:#888888;  text-align:center;'>{w['id']}</div>"
                     winners_html += "</div>"
